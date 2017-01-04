a430d0d68f700b519cc3bd5080bb8989540e6781
杨佩将 <ypj6024@ly.com>
2017-01-04 16:44:11 +0800

    super key word demo

---
+++ src/iandi/Subclass.java
@@ -0,0 +1,13 @@
+package iandi;
+
+public class Subclass extends Supperclass{
+	// overrides printMethod in Superclass
+    public void printMethod() {
+        super.printMethod();
+        System.out.println("Printed in Subclass");
+    }
+    public static void main(String[] args) {
+        Subclass s = new Subclass();
+        s.printMethod();    
+    }
+}
---
+++ src/iandi/Supperclass.java
@@ -0,0 +1,7 @@
+package iandi;
+
+public class Supperclass {
+	public void printMethod(){
+		System.out.print("Printed in SupperClass. ");
+	}
+}
