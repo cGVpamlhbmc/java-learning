74835c8a542d958ec77a973d70985f4b57f78754
pj.y <peijiang.yang@gmail.com>
2017-01-05 19:19:54 +0800

    Object class demo

---
+++ src/iandi/ObjectClass.java
@@ -0,0 +1,12 @@
+package iandi;
+
+public class ObjectClass {
+	
+	
+	public static void main(String[] args){
+		ObjectClass obj = new ObjectClass();
+		System.out.println(obj.getClass().getName());
+		System.out.println(obj.hashCode());
+		System.out.println(obj.toString());
+	}
+}
