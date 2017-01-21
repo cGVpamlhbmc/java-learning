74a2ebe9d55c84d55cfe235266782ae46d8cf3ab
pj.y <peijiang.yang@gmail.com>
2017-01-22 01:14:50 +0800

    scanner

---
+++ src/IOStreams/ScanXan.java
@@ -0,0 +1,22 @@
+package IOStreams;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Scanner;
+
+public class ScanXan {
+	public static void main(String[] args) throws FileNotFoundException {
+		Scanner s = null;
+		try {
+			s = new Scanner(new BufferedReader(new FileReader("in.txt")));
+			// s.useDelimiter(",\\s*");
+			while (s.hasNext()) {
+				System.out.println(s.next());
+			}
+		} finally {
+			if (s != null)
+				s.close();
+		}
+	}
+}
