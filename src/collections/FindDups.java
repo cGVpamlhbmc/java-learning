b6fbc249770a118703a923980b82497b529422fb
pj.y <peijiang.yang@gmail.com>
2017-02-23 00:16:12 +0800

    set

---
+++ src/collections/FindDups.java
@@ -0,0 +1,20 @@
+package collections;
+
+import java.util.*;
+import java.util.stream.*;
+
+public class FindDups {
+
+	public static void main(String[] args) {
+		// Set<String> distinctWords =
+		// Arrays.asList(args).stream().collect(Collectors.toSet());
+		// System.out.println(distinctWords.size() + " distinct words: " +
+		// distinctWords);
+
+		Set<String> s = new HashSet<String>();
+		for (String a : args)
+			s.add(a);
+		System.out.println(s.size() + " distinct words: " + s);
+	}
+
+}
