74d130b97f13ebfc3d9c55e5122391bb01b9baed
pj.y <peijiang.yang@gmail.com>
2017-03-16 14:15:09 +0800

    network-socket,datagrams

---
+++ src/networking/QuoteServer.java
@@ -0,0 +1,11 @@
+package networking;
+
+import java.io.IOException;
+
+public class QuoteServer {
+
+	public static void main(String[] args) throws IOException {
+		new QuoteServerThread().start();
+	}
+
+}
